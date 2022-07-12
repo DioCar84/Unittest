4f796666670914b07738b88decdcfc45f6bddffb
Diogo Carvalho <dcar84@gmail.com>
2022-07-12 09:02:01 +0000

    Add mocking functionality and tests

--- test_student.py
+++ test_student.py
@@ -1,6 +1,7 @@
 import unittest
 from student import Student
 from datetime import timedelta
+from unittest.mock import patch
 
 class TestStudent(unittest.TestCase):
 
@@ -40,9 +41,32 @@ class TestStudent(unittest.TestCase):
 
     def test_apply_extension(self):
         print('test_apply_extension')
+        old_end_date = self.student.end_date
         self.student.apply_extension(30)
 
-        self.assertNotEqual(self.student.end_date, self.student._start_date + timedelta(days=365))
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=30))
+
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+
+    def test_course_schedule_failed(self):
+        """
+        In the path "student" comes from the name of the file student.py
+        If you have named the file something else - it will need to match
+        eg, students.py would need "students.requests.get"
+        """
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong")
 
 
 if __name__ == '__main__':
